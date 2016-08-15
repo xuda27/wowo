61f21b2e36bc8c78874a56e505cc674109c11e8c
xuda <xuda27@163.com>
2016-08-15 10:46:47 +0800

    摄像头可以使用

--- workspace/wowo/WebContent/js/camera.js
+++ workspace/wowo/WebContent/js/camera.js
@@ -13,9 +13,8 @@ function isChromiumVersionLower() {
 }
 
 function successsCallback(stream) {
-	document.getElementById('camera_errbox').style.display = 'none';
 	document.getElementById('navy_video').src = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(stream): stream;
-
+	playPhoto();
 }
 
 function errorCallback(err) {
@@ -32,52 +31,15 @@ function accessLocalWebCam(id) {
 	}
 }
 
-
-window.addEventListener("DOMContentLoaded", function() {
+function playPhoto(){
 	var canvas = document.getElementById("canvas"),
 	context = canvas.getContext("2d"), video = document.getElementById("navy_video"), videoObj = {
 		"video" : true
 	}, errBack = function(error) {
 		console.log("相机调用失败...", error.code);
 	};
-
-	if (navigator.getUserMedia) {
-		navigator.getUserMedia(videoObj, function(stream) {
-			video.src = stream;
-			video.play();
-		}, errBack);
-	} else if (navigator.webkitGetUserMedia) {
-		navigator.webkitGetUserMedia(videoObj, function(stream) {
-			video.src = window.webkitURL.createObjectURL(stream);
-			video.play();
-		}, errBack);
-	}
-
+	
 	document.getElementById("snap").addEventListener("click", function() {
 		context.drawImage(video, 0, 0,$("#canvas").width(), $("#canvas").height());
 	});
-}, false);
-
-
-function convertCanvasToImage() {
-	var pic = document.getElementById("canvas").toDataURL("image/png");
-	pic = pic.replace(/^data:image\/(png|jpg);base64,/, "")
-
-	$.ajaxFileUpload({
-		url:'uploadServlet',
-		//fileElementId:'canvas',
-		data:{imageData:pic},
-		dataType:'json',
-		success:function(data,status){
-			data=parseInt( $.trim(data));
-			if(data==1){
-				alert("图片上传成功...");
-			}else{
-				alert("图片上传失败...");
-			}
-		},
-		error:function(data,status,e){
-			alert("图片上传失败...");
-		}
-	});
-}
+};
